f508cf7ab9f199e22d882bdd17af20414999deb0
Mohamed <Dinawi@me.com>
2022-05-09 21:19:55 +0200

    added files

--- src/test/java/com/sample/app/arithmetic/CalculatorTest.java
+++ src/test/java/com/sample/app/arithmetic/CalculatorTest.java
@@ -6,7 +6,7 @@ import static org.testng.Assert.*;
 public class CalculatorTest {
 
     @Test
-    public void add_10And20_30() {
+    public void add() {
         Calculator calculator = new Calculator();
 
         int result = calculator.add(100, 200);
@@ -15,7 +15,7 @@ public class CalculatorTest {
     }
 
     @Test
-    public void subtract_10And20_30() {
+    public void subtract() {
         Calculator calculator = new Calculator();
 
         int result = calculator.subtract(100, 200);
@@ -24,7 +24,7 @@ public class CalculatorTest {
     }
 
     @Test
-    public void mul_10And20_200() {
+    public void mul() {
         Calculator calculator = new Calculator();
 
         int result = calculator.mul(100, 200);
